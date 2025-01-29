11c12096c135a4cbff9848a884844cd1720f0be5
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-01-29 13:30:00 +0600

    update

--- L2/Session1/App.js
+++ L2/Session1/App.js
@@ -1,23 +1,6 @@
-//nested things
-/*
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-<div id="parent">
-    <div id="child">
-        <h1> I am H1 tag</h1>
-        <h2> I am H2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1> I am H1 tag</h1>
-        <h2> I am H2 tag</h2>
-    </div>
-<div>
-
-
-
-
-//reactElement(give object)  => convert => HTML(understand by the Browser)
-
-*/
 
 const parent = React.createElement(
   "div",
