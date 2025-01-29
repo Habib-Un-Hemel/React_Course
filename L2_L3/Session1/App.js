746e795020ef69e12ecb299d534f2d63125cd3ba
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-01-29 22:31:59 +0600

    update

--- L2_L3/Session1/App.js
+++ L2_L3/Session1/App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.creatElement(object) => Render in the Dom then => becomes a HTML
+//React.creatElement=> React Element - JS object => Render in the Dom as HTML element
 const heading = React.createElement("h1", { id: "heading" }, "HemelReact");
 
-const jsxHeading = <h1 id="heading">HemelReact with the help of JSX</h1>;
+// JSX=> React.creatElement=> React Element - JS object => Render in the Dom as HTML element
 
+//****React Element
+const jsxHeading = (
+  <h1 className="head" tabIndex={5}>
+    HemelReact with the help of JSX
+  </h1>
+);
+
+////****React Component
+const HeadingComponet = () => {
+  return <h1>This is the first Component write by Riha</h1>;
+};
+
+const HeadingComponet3 = () => {
+  return <h1>This is the first Component write by Riha1</h1>;
+};
+
+////****React Component syntax2
+const HeadingComponet2 = () => (
+  <div id="container">
+    <HeadingComponet3></HeadingComponet3>
+    <h1 className="heading">This is the first Component write by Hemel2</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
-
-// heading is React element which is a JS object, ReactDOM libray take the object and conver it into HTML when root.render is called
+root.render(<HeadingComponet2></HeadingComponet2>);
