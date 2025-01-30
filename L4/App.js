e0d2b8a6dd3537edebbb3f69635df001a6c52c4c
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-01-30 13:47:45 +0600

    done with header

--- L4/App.js
+++ L4/App.js
@@ -1,5 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/*
+*Header
+    - Logo
+    - Nav Items
+*Body
+    - Search
+    - ResturantContainer
+        - Resturant Card
+*Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<AppLayout />);
